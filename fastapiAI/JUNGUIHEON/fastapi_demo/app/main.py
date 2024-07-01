8c67a09e9ff4d319ec50ea7b5c5239b6ca80fe8d
JUNGUIHEON <jungui4366@gmail.com>
2024-07-01 09:35:40 +0900

    feat: [정의헌] Gradient Descent 훈련데이터 생성 [SKAIPx]

--- fastapiAI/JUNGUIHEON/fastapi_demo/app/main.py
+++ fastapiAI/JUNGUIHEON/fastapi_demo/app/main.py
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from async_db.database import getMySqlPool, createTableIfNecessary
 from exponential_regression.controller.exponential_regression_controller import exponentialRegressionRouter
+from gradient_descent.controller.gradient_descent_controller import gradientDescentRouter
 from kmeans.controller.kmeans_controller import kmeansRouter
 from logistic_regression.controller.logistic_regression_controller import logisticRegressionRouter
 from polynomialRegression.controller.polynomial_regression_controller import polynomialRegressionRouter
@@ -109,6 +110,7 @@ app.include_router(postRouter, prefix="/post")
 app.include_router(kmeansRouter)
 app.include_router(tfIrisRouter)
 app.include_router(ordersAnalysisRouter)
+app.include_router(gradientDescentRouter)
 
 load_dotenv()
 

6c7e146e51e62118c841655bba1a6a068755fd66
JUNGUIHEON <jungui4366@gmail.com>
2024-08-06 10:12:39 +0900

    feat: [정의헌] sequence analysis 중간 정리 [SKAIPx]

---
+++ fastapiAI/JUNGUIHEON/fastapi_demo/sequence_analysis/service/sequence_analysis_service_impl.py
@@ -0,0 +1,13 @@
+from sequence_analysis.repository.sequence_analysis_repository_impl import SequenceAnalysisRepositoryImpl
+from sequence_analysis.service.sequence_analysis_service import SequenceAnalysisService
+
+class SequenceAnalysisServiceImpl(SequenceAnalysisService):
+
+    def __init__(self):
+        self.sequenceAnalysisRepository = SequenceAnalysisRepositoryImpl()
+
+    def predictNextSequence(self, userSendMessage):
+        tokenizer = self.sequenceAnalysisRepository.createTokenizer(userSendMessage)
+        self.sequenceAnalysisRepository.extractSequence(tokenizer, userSendMessage)
+
+        return None
